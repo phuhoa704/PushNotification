d7200fda200debbcaf682538e4dff457a3c6ca2c
phuhoa704 <phuhoa704@gmail.com>
2022-08-06 11:26:05 +0700

    remove comment code

--- BT1/controllers/notification/index.js
+++ BT1/controllers/notification/index.js
@@ -98,59 +98,6 @@ const pushNotification = (async (req, res) => {
         })
     }
 })
-/* 
-const pushSingleToken = (async (req, res) => {
-    try {
-        const data = {};
-        data['title'] = req.body.title;
-        data['body'] = req.body.body;
-        data['device_token'] = req.body.token;
-        data['image'] = req.body.image;
-        data['icon'] = req.body.icon;
-        data['url'] = req.body.url;
-        await notificationService.pushSingleToken({ ...data, time: req.body.time });
-        res.json(await notificationService.create(data));
-    } catch (err) {
-        console.log(err);
-        res.json({
-            error: true,
-            message: err
-        })
-    }
-});
-
-const pushMultipleToken = (async (req, res) => {
-    try {
-        const data = {};
-        data['title'] = req.body.title;
-        data['body'] = req.body.body;
-        data['device_token'] = req.body.tokens;
-        data['image'] = req.body.image;
-        data['icon'] = req.body.icon;
-        data['url'] = req.body.url;
-        await notificationService.pushMultipleToken({ ...data, time: req.body.time })
-        data?.device_token?.forEach(token => {
-            const item = {};
-            item['device_token'] = token;
-            item['title'] = data.title;
-            item['body'] = data.body;
-            item['image'] = data.image;
-            item['icon'] = data.icon;
-            item['url'] = data.url;
-            notificationService.create(item)
-        })
-        res.json({
-            errorCode: '200',
-            message: 'Push successfully'
-        })
-    } catch (err) {
-        console.log(err);
-        res.json({
-            error: true,
-            message: err
-        })
-    }
-}) */
 
 module.exports = {
     getAll, pushNotification, subscribeToTopic, unsubscribeFromTopic, sendPushToTopic
--- BT1/services/notification.js
+++ BT1/services/notification.js
@@ -90,64 +90,6 @@ class NotificationService extends BaseService {
                 });
             }, data?.time);
     }
-/* 
-    pushSingleToken = async (data) => {
-        console.log(`===============${this.constructor.name}, call method PushSingleToken ==============`);
-        let message = {
-            webpush: {
-                notification: {
-                    title: data.title,
-                    body: data.body,
-                    image: data.image,
-                    icon: data.icon
-                },
-            },
-            token: data.device_token,
-            data: {
-                url: data.url,
-                body: data.body
-            }
-        };
-        setTimeout(() => {
-            fcm.send(message, function (err, response) {
-                if (err) {
-                    console.log("Something has gone wrong!")
-                    console.log(err)
-                } else {
-                    console.log("Successfully sent with response: ", response)
-                }
-            })
-        }, data?.time);
-    }
-
-    pushMultipleToken = async (data) => {
-        console.log(`===============${this.constructor.name}, call method PushMultipleToken ==============`);
-        let tokens = data?.device_token;
-        let message = {
-            webpush: {
-                notification: {
-                    title: data.title,
-                    body: data.body,
-                    image: data.image,
-                    icon: data.icon
-                },
-            },
-            data: {
-                url: data.url,
-                body: data.body
-            }
-        };
-        setTimeout(() => {
-            fcm.sendToMultipleToken(message, tokens, function (err, response) {
-                if (err) {
-                    console.log("Something has gone wrong!")
-                    console.log(err)
-                } else {
-                    console.log("Successfully sent with response: ", response)
-                }
-            });
-        }, data?.time);
-    } */
 }
 
 module.exports = { NotificationService }
